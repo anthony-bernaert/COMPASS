59dbf2f615636c122c7adaff0264032f065ac2ed
Anthony Bernaert <anthony.bernaert@gmail.com>
2024-09-24 21:02:29 +0200

    List view: add description column and show in explorer button
    Signed-off-by: Anthony Bernaert <anthony.bernaert@gmail.com>

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace COMPASS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -128,6 +128,30 @@ namespace COMPASS.Properties {
             }
             set {
                 this["ListShowEditIcon"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool ListShowDescription {
+            get {
+                return ((bool)(this["ListShowDescription"]));
+            }
+            set {
+                this["ListShowDescription"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool ListShowPath {
+            get {
+                return ((bool)(this["ListShowPath"]));
+            }
+            set {
+                this["ListShowPath"] = value;
             }
         }
         
